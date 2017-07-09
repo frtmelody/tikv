84a5c8cccfcc588c38de5007ca57451fec563d07
Cholerae Hu <huyingqian@pingcap.com>
2017-07-09 15:37:33 +0800

    scheduler: fix ts initial value
    Signed-off-by: Cholerae Hu <huyingqian@pingcap.com>

--- src/storage/txn/scheduler.rs
+++ src/storage/txn/scheduler.rs
@@ -36,6 +36,7 @@ use std::fmt::{self, Formatter, Debug};
 use std::sync::mpsc::Receiver;
 use std::time::Duration;
 use std::thread;
+use std::u64;
 
 use threadpool::ThreadPool;
 use prometheus::HistogramTimer;
@@ -357,7 +358,7 @@ fn process_read(cid: u64, mut cmd: Command, ch: SyncSendCh<Msg>, snapshot: Box<S
                                              true,
                                              None,
                                              ctx.get_isolation_level());
-            let mut ts: u64 = 0;
+            let mut ts: u64 = u64::MAX;
             let mut mvccs = vec![];
             let mut err: Option<StorageError> = None;
             loop {
@@ -369,7 +370,7 @@ fn process_read(cid: u64, mut cmd: Command, ch: SyncSendCh<Msg>, snapshot: Box<S
                     Ok(opt) => {
                         match opt {
                             Some((commit_ts, write)) => {
-                                ts = write.start_ts + 1;
+                                ts = commit_ts - 1;
                                 mvccs.push((commit_ts, write) as MvccPair);
                             }
                             None => break,
@@ -395,7 +396,7 @@ fn process_read(cid: u64, mut cmd: Command, ch: SyncSendCh<Msg>, snapshot: Box<S
                 Ok(opt) => {
                     match opt {
                         Some((key, _)) => {
-                            let mut ts: u64 = 0;
+                            let mut ts: u64 = u64::MAX;
                             let mut mvccs = vec![];
                             let mut err: Option<StorageError> = None;
                             loop {
@@ -407,7 +408,7 @@ fn process_read(cid: u64, mut cmd: Command, ch: SyncSendCh<Msg>, snapshot: Box<S
                                     Ok(opt) => {
                                         match opt {
                                             Some((commit_ts, write)) => {
-                                                ts = write.start_ts + 1;
+                                                ts = commit_ts - 1;
                                                 mvccs.push((commit_ts, write) as MvccPair);
                                             }
                                             None => break,
